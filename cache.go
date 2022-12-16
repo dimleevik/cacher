4ef232f7819f5e49eab1e09c9acce6da0994dd18
Dmitriy <dimleevik@gmail.com>
2022-12-16 11:06:40 +0500

    init

---
+++ cache.go
@@ -0,0 +1,22 @@
+package cache
+
+type Cache struct {
+	storage map[string]interface{}
+}
+
+func New() Cache {
+	return Cache{make(map[string]interface{})}
+}
+
+func (c Cache) Get(key string) (interface{}, bool) {
+	value, ok := c.storage[key]
+	return value, ok
+}
+
+func (c *Cache) Set(key string, val interface{}) {
+	c.storage[key] = val
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.storage, key)
+}
